0a8eb2ae0e3ead17ad2cad4158649493255d216a
Rene Breton <r.breton@soton.ac.uk>
2013-06-06 13:43:48 +0100

    Fix a few minor things.

--- pysovo/__init__.py
+++ pysovo/__init__.py
@@ -23,14 +23,6 @@ except Exception as e:
     logger.warn("Could not load contacts file; reason:\n" + str(e))
     contacts = {}
 
-try:
-    default_email_account = comms.email.load_account_settings_from_file(
-                                                    default_email_config_file)
-    logger.debug("Default email account loaded from %s", default_email_config_file)
-except Exception as e:
-    logger.warn("Could not load default email account; reason:\n", +str(e))
-    default_email_account = None
-
 
 
 
